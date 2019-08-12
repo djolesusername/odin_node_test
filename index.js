2b13a3f6f4192ec5ecdb1789a44664fd98b73dfc
Djordje Cvetkovic <44837430+djolesusername@users.noreply.github.com>
2019-08-12 12:23:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var server = http.createServer(function (req, res) {
         fs.createReadStream(__dirname + '/about.html').pipe(res);
     }
     else {
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writeHead(404, { "Content-Type": "text/html" });
         fs.createReadStream(__dirname + '/404.html').pipe(res);
 
     }
